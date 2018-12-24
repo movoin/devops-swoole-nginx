87157708a8755ef5cd3a2e13d30f12519e70dc05
Allen Luo <movoin@gmail.com>
2018-12-24 21:55:20 +0800

    fixed bugs

--- versions/4/conf/bin/service.d/nginx.d/10-init.sh
+++ versions/4/conf/bin/service.d/nginx.d/10-init.sh
@@ -9,20 +9,14 @@ fi
  ##
 source "$DOCKER_CONF_PATH/bin/functions.sh"
 
-if [ ! -d "$DOCKER_CONF_PATH/.cache/nginx" ];then
-	mkdir -p $DOCKER_CONF_PATH/.cache/nginx
-	# Backup
-	cp $DOCKER_CONF_PATH/etc/nginx/*.conf $DOCKER_CONF_PATH/.cache/nginx
-else
-	cp -f $DOCKER_CONF_PATH/.cache/nginx/*.conf $DOCKER_CONF_PATH/etc/nginx/
-fi
-
 # Replace markers
 replaceFile "<DOCUMENT_INDEX>"		"$WEB_DOCUMENT_INDEX" 	"$DOCKER_CONF_PATH/etc/nginx"
 replaceFile "<DOCUMENT_ROOT>"		"$WEB_DOCUMENT_ROOT" 	"$DOCKER_CONF_PATH/etc/nginx"
 replaceFile "<ALIAS_DOMAIN>"		"$WEB_ALIAS_DOMAIN" 	"$DOCKER_CONF_PATH/etc/nginx"
 replaceFile "<SERVERNAME>"			"$HOSTNAME" 			"$DOCKER_CONF_PATH/etc/nginx"
 replaceFile "<DOCKER_CONF_PATH>"	"$DOCKER_CONF_PATH" 	"$DOCKER_CONF_PATH/etc/nginx"
+replaceFile "<DOCKER_CONF_PATH>"	"$DOCKER_CONF_PATH" 	"/etc/nginx"
+replaceFile "<DOCKER_CONF_PATH>"	"$DOCKER_CONF_PATH" 	"/etc/nginx/conf.d"
 
 if [[ -n "${WEB_PHP_SOCKET+x}" ]]; then
 	## WEB_PHP_SOCKET is set
